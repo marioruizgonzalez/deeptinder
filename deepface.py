3f7f178d112ee094b5efb1e7b41062f5644047c8
Mario Ruiz <>
2022-12-07 18:45:00 -0600

    subiendo

--- deepface.py
+++ deepface.py
@@ -1,4 +1,29 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+
+from flask import Flask, jsonify, request, make_response
+
+import argparse
+import uuid
+import json
+import time
+from tqdm import tqdm
+
+import tensorflow as tf
+tf_version = int(tf.__version__.split(".")[0])
+
+if tf_version == 2:
+	import logging
+	tf.get_logger().setLevel(logging.ERROR)
+
+#------------------------------
+
 from deepface import DeepFace
+
 import cv2
 import matplotlib.pyplot as plt
 import pandas as pd
